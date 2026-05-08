d88118b24794adb6171c6a29cdb79dce8d7a0110
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-05-08 10:33:27 +0530

    Hoisting

---
+++ lec13/hoisting.js
@@ -0,0 +1,32 @@
+sum()
+function sum() {
+    console.log("hii");
+    
+}
+
+//
+// function sum() {
+//     console.log("hii");
+    
+// }
+// sum()
+
+
+console.log(a)
+var a=5
+
+
+// var a=5
+// console.log(a)
+
+// summm()
+// var summm=()=>{  // when any arrow functn is call before declaration then error will come
+//     console.log("hello");//when its key is varr then error is summm is not a function
+    
+// }
+
+// summ()
+// let summ=()=>{  // when any arrow functn is call before declaration then error will come
+//     console.log("hello");//when its key is let or const then error can't access summ brforee initialisation
+    
+// }
--- lec13/hosting.js
+++
@@ -1,9 +0,0 @@
-sum()
-function sum() {
-    console.log("hii");
-    
-}
-
-var a
-console.log(a)
-a=5
