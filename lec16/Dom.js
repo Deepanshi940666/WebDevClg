4bc26842fa7dec0b36fa19558af14b2a426d77bb
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-05-12 11:24:04 +0530

    DOM Property

--- lec16/Dom.js
+++ lec16/Dom.js
@@ -3,5 +3,19 @@ for(let val of H1){
     val.style.color="red"
 }
 let name=document.getElementById("Name")
-let nameData= prompt("Enter your name")
-name.innerText= "Name: "+nameData
+// let nameData= prompt("Enter your name")
+// name.innerText= "Name: "+nameData
+
+//--------Input------------
+let inp=document.getElementById("input")
+let btn=document.querySelector("button")
+let H2=document.querySelector("h2")
+inp.addEventListener("input",(e)=>{
+    btn.addEventListener("click",(f)=>{
+        H2.innerText=e.target.value
+    })
+    
+})
+
+
+
