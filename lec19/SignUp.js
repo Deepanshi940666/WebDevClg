0422a91cd4f1286c5e638b76cc9ab5dfdc69258d
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-05-18 12:58:31 +0530

    Form

--- lec19/SignUp.js
+++ lec19/SignUp.js
@@ -1,6 +1,8 @@
 let SignData = document.querySelector("#Form");
 let LoginData = document.querySelector("#LoginForm");
-
+let h1=document.querySelector("#h1")
+let h2=document.querySelector("#h2")
+let h3=document.querySelector("#h3")
 
 // let Submit=document.querySelector("#submit")
 // console.log(SignData);
@@ -17,6 +19,8 @@ SignData.addEventListener("submit", (e) => {
     };
     localStorage.setItem("UserData", JSON.stringify(data));
 });
+
+
 LoginData.addEventListener("submit", (e) => {
     e.preventDefault();
      let Sign = localStorage.getItem("UserData");
@@ -26,16 +30,28 @@ LoginData.addEventListener("submit", (e) => {
         Email: LoginData[1].value,
         Password: LoginData[2].value
     };
+   
     
     // localStorage.setItem("LoginData", JSON.stringify(data));
 
    
     // let login = localStorage.getItem("LoginData");
 
-    if (SignUpData.name===data.name && SignUpData.Email===data.Email && SignUpData.Password===data.Password ) {
+    if (SignUpData.name===data.name && SignUpData.Email===data.email && SignUpData.password===data.Password ) {
         alert("Login.... Done")
     }
     else{
         alert("Incorrect Input.....")
     }
+
+   
 });
+
+let Sign = localStorage.getItem("UserData");
+let SignUpData =JSON.parse(Sign);
+
+if(SignUpData){
+    h1.innerText= SignUpData.name
+    h2.innerText= SignUpData.email
+    h3.innerText= SignUpData.password
+}
