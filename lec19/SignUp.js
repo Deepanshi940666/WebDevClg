c1e993d2cc999f0fe09fe4be8385c1af9a36fd5d
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-05-18 11:43:06 +0530

    Form

--- lec19/SignUp.js
+++ lec19/SignUp.js
@@ -1,34 +1,41 @@
-let SignData= document.querySelector("#Form")
+let SignData = document.querySelector("#Form");
+let LoginData = document.querySelector("#LoginForm");
+
+
 // let Submit=document.querySelector("#submit")
 // console.log(SignData);
 
-SignData.addEventListener("submit",(e)=>{
-    e.preventDefault()
+SignData.addEventListener("submit", (e) => {
+    e.preventDefault();
     // console.log(SignData[0].value);
     // console.log(SignData[1].value);
     // console.log(SignData[2].value);
-    let data={
+    let data = {
         name: SignData[0].value,
-        Email: SignData[1].value,
-        Password: SignData[2].value
-    }
-    localStorage.setItem("UserData",JSON.stringify(data))
-})
-
-let LoginData=document.addEventListener("submit",(e)=>{
-    e.preventDefault()
-     let data={
-        name: SignData[0].value,
-        Email: SignData[1].value,
-        Password: SignData[2].value
-    }
-    localStorage.setItem("LoginData",JSON.stringify(data))
-})
-
-let Sign= localStorage.getItem("UserData")
-let login= localStorage.getItem("LoginData")
-
-if(Sign===login){
-    console.log("Done");
+        email: SignData[1].value,
+        password: SignData[2].value,
+    };
+    localStorage.setItem("UserData", JSON.stringify(data));
+});
+LoginData.addEventListener("submit", (e) => {
+    e.preventDefault();
+     let Sign = localStorage.getItem("UserData");
+     let SignUpData =JSON.parse(Sign)
+    let data = {
+        name: LoginData[0].value,
+        Email: LoginData[1].value,
+        Password: LoginData[2].value
+    };
     
-}
+    // localStorage.setItem("LoginData", JSON.stringify(data));
+
+   
+    // let login = localStorage.getItem("LoginData");
+
+    if (SignUpData.name===data.name && SignUpData.Email===data.Email && SignUpData.Password===data.Password ) {
+        alert("Login.... Done")
+    }
+    else{
+        alert("Incorrect Input.....")
+    }
+});
