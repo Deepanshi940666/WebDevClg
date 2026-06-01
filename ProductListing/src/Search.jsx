b2fa99a48dbe4614c6dd68024e7b1fafadadcaa6
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-01 11:24:40 +0530

    Product Listing

--- ProductListing/src/Search.jsx
+++ ProductListing/src/Search.jsx
@@ -1,28 +1,70 @@
 import React, { useEffect } from 'react'
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css'
 const Search = () => {
-let [input,setInput]=useState("")
-console.log("hiii");
+    let [input, setInput] = useState("")
+    let [ApiData, setApiData] = useState([])
+    console.log("hiii");
 
-async function ApiCall() {
-    if(input.trim()){
-        alert("Enter something in searchbar")
-        return
+    async function ApiCall() {
+        console.log("heyyyyyy");
+        console.log(input, "imppppp");
+
+
+        if (!input.trim()) {
+            alert("Enter something in searchbar")
+            return
+        }
+        let res = await fetch(`https://dummyjson.com/products/search?q=${input}`)
+        let data = await res.json()
+        console.log(data);
+        setApiData(data.products)
+
     }
-    let res=await fetch(`https://dummyjson.com/products/search?q=${input}`)
-    let data= await res.json()
-    console.log(data);
-    
-}
 
 
-  return (
-    <div id='searchBar'>
-        <input id='searching' type='text' placeholder='Enter Product' onChange={(e)=>{console.log(e.target.value)}}></input>
-        <button id='searchBtn'  onClick={ApiCall}>Search</button>
-    </div>
-  )
+    useEffect(() => {
+        async function call() {
+            let res = await fetch("https://dummyjson.com/products")
+            let data = await res.json()
+            // console.log(data);
+            setApiData(data.products);
+            // console.log(ApiData);
+
+        }
+        call()
+    }, [])
+
+
+
+    return (
+        <div>
+
+            <div id='searchBar'>
+                <input id='searching' type='text' placeholder='Enter Product' onChange={(e) => { setInput(e.target.value) }}></input>
+                <button id='searchBtn' onClick={ApiCall}>Search</button>
+
+
+            </div>
+            <div id="Div" >
+                {/* <search/> */}
+                {
+
+                    ApiData.map((a) => {
+                        return (<div class="Boxes">
+                            <img src={a.thumbnail}></img>
+                            <h1>{a.id}</h1>
+                            <h5>{a.title}</h5>
+
+                        </div>)
+                    })
+                }
+                {/* <button></button> */}
+
+            </div>
+        </div>
+
+    )
 }
 
 export default Search
