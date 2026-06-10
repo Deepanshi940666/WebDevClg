7700c3dcca6ec6783338fb125016327a36839c31
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-10 22:04:31 +0530

    App and main file update

--- ToDoByContext/ToDo/src/App.jsx
+++ ToDoByContext/ToDo/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import Home from './ToDOListContext/Home'
+// import Home from './ToDOListContext/Home'
 
+// import Contexttt from './Context_todo/todo_Context'
+// import Todo from "./Context_todo/todo"
+import './ThemeMode/Bg.css'
+import Bg from './ThemeMode/Bg'
 const App = () => {
   return (
     <div>
-      <Home/>
+      {/* <Contexttt/> */}
+      {/* <Todo/> */}
+      <Bg/>
     </div>
   )
 }
--- ToDoByContext/ToDo/src/main.jsx
+++ ToDoByContext/ToDo/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+import Context from "./Context_todo/todo_Context"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Context>
     <App />
+    </Context>
+   
   </StrictMode>,
 )
